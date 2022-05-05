bbcb9c261ac0813f0dc117db017ba8866232c4d4
VighneshBilgi <vhbilgi@gmail.com>
2022-05-05 15:23:12 +0530

    Added: UC1_Attendance_Check

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,6 +2,15 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Wage Computation Program");
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        int attendance = (int)(Math.random()*10)%2;
+        if (attendance == 0){
+            System.out.println("Employee is absent");
+        }
+        else {
+            System.out.println("Employee is present");
+        }
+
     }
 }

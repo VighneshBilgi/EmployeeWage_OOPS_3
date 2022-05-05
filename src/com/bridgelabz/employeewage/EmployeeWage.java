76523867f37bf947c4807adfc0e200d190078667
VighneshBilgi <vhbilgi@gmail.com>
2022-05-05 15:40:44 +0530

    Added: UC5_Wage_In_A_Month

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,28 +5,33 @@ public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_DAY_HOUR = 4;
+    static final int TOTAL_WORK_DAY = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int dailyWage =0;
+        int dayCount = 1;
 
-        int attendance = (int)(Math.random()*10)%3;
+        while (dayCount<TOTAL_WORK_DAY) {
 
-        switch (attendance){
-            case 1 :
-                System.out.println("Employee is present Full Time");
-                dailyWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
-                break;
-            case 2 :
-                System.out.println("Employee is present Part Time");
-                dailyWage = WAGE_PER_HOUR*PART_DAY_HOUR;
-                break;
-            default:
-                System.out.println("Employee is absent");
+            int dailyWage = 0;
+            int attendance = (int) (Math.random() * 10) % 3;
+
+            switch (attendance) {
+                case 1:
+                    System.out.println("Employee is present Full Time");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+                case 2:
+                    System.out.println("Employee is present Part Time");
+                    dailyWage = WAGE_PER_HOUR * PART_DAY_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+            }
+
+            System.out.println("Daily Wage on Day :" + dayCount + " is :" +dailyWage);
+            dayCount++;
         }
-
-
-        System.out.println("Daily Wage :"+dailyWage);
     }
 }

97dcff4d75a3d9874b6c9aaed19fd4aa6ee929dd
VighneshBilgi <vhbilgi@gmail.com>
2022-05-05 15:31:19 +0530

    Added: UC3_Part_Time_Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,19 +4,22 @@ public class EmployeeWage {
 
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PART_DAY_HOUR = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int dailyWage =0;
 
-        int attendance = (int)(Math.random()*10)%2;
+        int attendance = (int)(Math.random()*10)%3;
         if (attendance == 0){
             System.out.println("Employee is absent");
-        }
-        else {
-            System.out.println("Employee is present");
+        } else if (attendance == 1) {
+            System.out.println("Employee is present Full Time");
             dailyWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
+        } else {
+            System.out.println("Employee is present Part Time");
+            dailyWage = WAGE_PER_HOUR*PART_DAY_HOUR;
         }
 
         System.out.println("Daily Wage :"+dailyWage);
